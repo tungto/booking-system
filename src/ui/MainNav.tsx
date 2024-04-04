9f7f662ba27c17c45567c35862d33b68790bc458
tungto <tung.to@informa.com>
2024-04-04 15:45:35 +0700

    style MainNav

--- src/ui/MainNav.tsx
+++ src/ui/MainNav.tsx
@@ -28,6 +28,29 @@ const StyledNavLink = styled(NavLink)`
 		padding: 1.2rem 2.4rem;
 		transition: all 0.3s;
 	}
+
+	&:active,
+	&:hover,
+	&:active:link,
+	&:active:visited {
+		color: var(--color-grey-800);
+		background-color: var(--color-grey-50);
+		border-radius: var(--border-radius-sm);
+	}
+
+	& svg {
+		width: 2.4rem;
+		height: 2.4rem;
+		color: var(--color-grey-400);
+		transition: all 0.3s;
+	}
+
+	&:hover svg,
+	&:active svg,
+	&.active:link svg,
+	&.active:visited svg {
+		color: var(--color-brand-600);
+	}
 `;
 
 const MainNav = () => {
